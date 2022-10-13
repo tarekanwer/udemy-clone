d6a061ee2c5271f07e7d31c84c2d04f58d5c49ff
tarekanwer <tito.a7mad@gmail.com>
2022-10-13 06:01:18 +0200

    app init

---
+++ src/components/ui/Layout.jsx
@@ -0,0 +1,10 @@
+import React, {Fragment} from 'react'
+import classes from './Layout.module.css'
+
+const Layout = (props) => {
+  return (
+    <div className={classes.container}>{props.children}</div>
+  )
+}
+
+export default Layout
