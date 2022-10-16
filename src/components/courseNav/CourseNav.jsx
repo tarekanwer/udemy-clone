65dff22cba524ec16e0da4f828fff6ff22d2fefd
tarekanwer <tito.a7mad@gmail.com>
2022-10-16 10:25:02 +0200

    Course Nav init

---
+++ src/components/courseNav/CourseNav.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./CourseNav.module.css";
+
+const CourseNav = () => {
+  return <div className={classes.container}>CourseNav</div>;
+};
+
+export default CourseNav;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Layout from "../components/ui/Layout";
 import Gallery from "../components/gallery/Gallery";
+import CourseNav from "../components/courseNav/CourseNav";
 
 const HomePage = (props) => {
-  return <Layout>
-    <Gallery/>
-  </Layout>;
+  return (
+    <Layout>
+      <Gallery />
+      <CourseNav />
+    </Layout>
+  );
 };
 
 export default HomePage;
