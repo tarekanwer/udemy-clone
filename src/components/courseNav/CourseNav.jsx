170ef753d0c2c406328558fde836be3fca72af01
tarekanwer <tito.a7mad@gmail.com>
2022-10-17 05:32:54 +0200

    nav init

--- src/components/courseNav/CourseNav.jsx
+++ src/components/courseNav/CourseNav.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import classes from "./CourseNav.module.css";
+import Nav from "./Nav";
 
 const CourseNav = () => {
-  return <div className={classes.container}>CourseNav</div>;
+  return (
+    <div className={classes.container}>
+      <h1>A broad selection of courses</h1>
+      <p>
+        Choose from 204,000 online video courses with new additions published
+        every month
+      </p>
+      <Nav />
+    </div>
+  );
 };
 
 export default CourseNav;
