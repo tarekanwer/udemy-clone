139ec658711029e5fa987f1a988a5e2be63b2576
tarekanwer <tito.a7mad@gmail.com>
2022-10-18 20:57:00 +0200

    Course Box init

---
+++ src/components/courseNav/Category.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './Category.module.css';
+
+const Category = () => {
+  return (
+    <div className={classes.container}>Category</div>
+  )
+}
+
+export default Category
--- src/components/courseNav/Nav.jsx
+++ src/components/courseNav/Nav.jsx
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
 import classes from "./Nav.module.css";
+import Category from "./Category";
+
 const categories = [
   "Python",
   "Excel",
@@ -9,34 +11,38 @@ const categories = [
   "AWS Certification",
   "Drawing",
 ];
+
 const Nav = () => {
   const [isSelected, setIsSelected] = useState(0);
   const classHandler = (event) => {
     setIsSelected(+event.target.value);
   };
   return (
-    <nav className={classes.container}>
-      <ul>
-        {categories.map((item, index) => {
-          return (
-            <li key={index}>
-              <button
-                key={index}
-                className={
-                  isSelected === index
-                    ? `${classes.btn} ${classes.active}`
-                    : `${classes.btn}`
-                }
-                onClick={classHandler}
-                value={index}
-              >
-                {item}
-              </button>
-            </li>
-          );
-        })}
-      </ul>
-    </nav>
+    <Fragment>
+      <nav className={classes.container}>
+          <ul>
+            {categories.map((item, index) => {
+              return (
+                <li key={index}>
+                  <button
+                    key={index}
+                    className={
+                      isSelected === index
+                        ? `${classes.btn} ${classes.active}`
+                        : `${classes.btn}`
+                    }
+                    onClick={classHandler}
+                    value={index}
+                  >
+                    {item}
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
+      </nav>
+      <Category />
+    </Fragment>
   );
 };
 
