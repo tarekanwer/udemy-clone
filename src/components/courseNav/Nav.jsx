90a6bc8ff24b028e7c37efaf6183f784b3a8e6d7
tarekanwer <tito.a7mad@gmail.com>
2022-10-18 18:15:12 +0200

    adding nav

--- src/components/courseNav/Nav.jsx
+++ src/components/courseNav/Nav.jsx
@@ -1,10 +1,44 @@
-import React from 'react';
-import classes from './Nav.module.css';
-
+import React, { useState } from "react";
+import classes from "./Nav.module.css";
+const categories = [
+  "Python",
+  "Excel",
+  "Web Development",
+  "Javascript",
+  "Data Science",
+  "AWS Certification",
+  "Drawing",
+];
 const Nav = () => {
+  const [isSelected, setIsSelected] = useState(0);
+  const classHandler = (event) => {
+    setIsSelected(+event.target.value);
+  };
+  console.log(isSelected);
   return (
-    <div className={classes.container}>Nav</div>
-  )
-}
+    <nav className={classes.container}>
+      <ul>
+        {categories.map((item, index) => {
+          return (
+            <li key={index}>
+              <button
+                key={index}
+                className={
+                  isSelected === index
+                    ? `${classes.btn} ${classes.active}`
+                    : `${classes.btn}`
+                }
+                onClick={classHandler}
+                value={index}
+              >
+                {item}
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
 
-export default Nav
+export default Nav;
