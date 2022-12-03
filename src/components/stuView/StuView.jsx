597d1e3fe9818525704339aa5bb069a461643bf1
tarekanwer <tito.a7mad@gmail.com>
2022-12-03 17:05:51 +0200

    stu views init

---
+++ src/components/stuView/StuView.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./StuView.module.css";
+
+const StuView = () => {
+  return <div>StuView</div>;
+};
+
+export default StuView;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Layout from "../components/ui/Layout";
 import Gallery from "../components/gallery/Gallery";
 import CourseNav from "../components/courseNav/CourseNav";
+import StuView from "../components/stuView/StuView";
 
 const HomePage = (props) => {
   return (
     <Layout>
       <Gallery />
       <CourseNav />
+      <StuView />
     </Layout>
   );
 };
