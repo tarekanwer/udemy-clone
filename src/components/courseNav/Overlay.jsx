936838b5095cedcc02d42ab57ac9d4ec827da8a2
tarekanwer <tito.a7mad@gmail.com>
2022-12-04 05:10:23 +0200

    styling overlays

--- src/components/courseNav/Overlay.jsx
+++ src/components/courseNav/Overlay.jsx
@@ -20,41 +20,43 @@ const Overlay = (props) => {
 
   return ReactDom.createPortal(
     <div className={classes.container} style={styles}>
-      <div className={classes.content}>
-        <h1>Learn Python: The Complete Python Programming Course</h1>
-        <h5>
-          Updated <b>September 2022</b>
-        </h5>
-        <h6>14.5 . total hoursAll . LevelsSubtitles</h6>
-        <h4>
-          Learn A-Z everything about Python, from the basics, to advanced topics
-          like Python GUI, Python Data Analysis, and more!{" "}
-        </h4>
-        <ul>
-          <li>
-            <DoneIcon sx={{ width: "1.15rem", height: "1.15rem" }} />{" "}
-            <h6>Create their own Python Programs</h6>
-          </li>
-          <li>
-            <DoneIcon sx={{ width: "1.15rem", height: "1.15rem" }} />{" "}
-            <h6>Become an experienced Python Programmer</h6>
-          </li>
-          <li>
-            <DoneIcon sx={{ width: "1.15rem", height: "1.15rem" }} />{" "}
-            <h6>Parse the Web and create their own Games</h6>
-          </li>
-        </ul>
-        <span>
-          <button className={classes.cart}>Add the cart</button>
-          <button className={classes.fav}>
-            <FavoriteBorderIcon
-              sx={{
-                width: "1.8rem",
-                height: "1.8rem",
-              }}
-            />
-          </button>
-        </span>
+      <div className={classes.border}>
+        <div className={classes.content}>
+          <h1>Learn Python: The Complete Python Programming Course</h1>
+          <h5>
+            Updated <b>September 2022</b>
+          </h5>
+          <h6>14.5 . total hoursAll . LevelsSubtitles</h6>
+          <h4>
+            Learn A-Z everything about Python, from the basics, to advanced
+            topics like Python GUI, Python Data Analysis, and more!{" "}
+          </h4>
+          <ul>
+            <li>
+              <DoneIcon sx={{ width: "1.15rem", height: "1.15rem" }} />{" "}
+              <h6>Create their own Python Programs</h6>
+            </li>
+            <li>
+              <DoneIcon sx={{ width: "1.15rem", height: "1.15rem" }} />{" "}
+              <h6>Become an experienced Python Programmer</h6>
+            </li>
+            <li>
+              <DoneIcon sx={{ width: "1.15rem", height: "1.15rem" }} />{" "}
+              <h6>Parse the Web and create their own Games</h6>
+            </li>
+          </ul>
+          <span>
+            <button className={classes.cart}>Add the cart</button>
+            <button className={classes.fav}>
+              <FavoriteBorderIcon
+                sx={{
+                  width: "1.8rem",
+                  height: "1.8rem",
+                }}
+              />
+            </button>
+          </span>
+        </div>
       </div>
     </div>,
     document.getElementById("overlays")
