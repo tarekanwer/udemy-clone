ed709505767ab3094de3c4968b09ddb133646316
tarekanwer <tito.a7mad@gmail.com>
2022-12-12 19:54:27 +0200

    cleaning

--- src/components/stuView/StuView.jsx
+++ src/components/stuView/StuView.jsx
@@ -3,10 +3,12 @@ import classes from "./StuView.module.css";
 import CategoryCourses from "../courseNav/CategoryCourses";
 const y = 1080;
 const StuView = () => {
-  return <div className={classes.container}>
-    <h1>Students are viewing</h1>
-    <CategoryCourses y = {y}/>
-  </div>;
+  return (
+    <div className={classes.container}>
+      <h1>Students are viewing</h1>
+      <CategoryCourses y={y} />
+    </div>
+  );
 };
 
 export default StuView;
