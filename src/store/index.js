7382ed3b70ce2fd498829cf11fe1370071f58633
tarekanwer <tito.a7mad@gmail.com>
2022-12-31 12:07:52 +0200

    format code

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,6 @@
 import { createStore } from "redux";
 const initState = {
   x: 0,
-  y: 0,
   xn: 0,
   R: false,
   L: false,
@@ -12,7 +11,6 @@ const overlayReducer = (state = initState, action) => {
     let position = action.position;
     let x = position.right * 1.01;
     let xn = position.left - 330;
-    // let y = 745;
     let overlay = action.overlay;
     let R;
     let L;
