8d4c8fccb883e409c888a78c2a1edf058e7f54c1
tarekanwer <tito.a7mad@gmail.com>
2023-01-09 19:16:16 +0200

    code clean

--- src/components/ui/Layout.jsx
+++ src/components/ui/Layout.jsx
@@ -1,9 +1,14 @@
-import React,{Fragment} from "react";
+import React, { Fragment } from "react";
 import classes from "./Layout.module.css";
 import Header from "../header/Header";
 
 const Layout = (props) => {
-  return <Fragment><Header/><div className={classes.container}>{props.children}</div></Fragment>;
+  return (
+    <Fragment>
+      <Header />
+      <div className={classes.container}>{props.children}</div>
+    </Fragment>
+  );
 };
 
 export default Layout;
